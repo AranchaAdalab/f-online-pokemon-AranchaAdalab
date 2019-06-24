2938b2965ab5eb81814ff437b8bffc0d59b344d4
ARANCHA NARVÁEZ ALONSO <aranzazunarvaez@gmail.com>
2019-06-24 13:51:28 +0200

    Update docs

---
+++ docs/precache-manifest.fc6e69fe869345b1daafb6414745c2ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be556474b84fe638bb086c33a67782a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5493611ce723e61679f4",
+    "url": "./static/css/main.a34dd257.chunk.css"
+  },
+  {
+    "revision": "814f492ef54ef64db444",
+    "url": "./static/js/2.5c444cd0.chunk.js"
+  },
+  {
+    "revision": "5493611ce723e61679f4",
+    "url": "./static/js/main.64d65a3b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9a00f048fa7cbb011bdf5ab94993aa8e.js"
+  "./precache-manifest.fc6e69fe869345b1daafb6414745c2ba.js"
 );
 
 self.addEventListener('message', (event) => {
